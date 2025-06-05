8a7c14f438efb09174519ca13e4bf5850571520d
Pachipulusu <GVSBhaskar.Pachipulusu@partner.magna.com>
2025-06-05 11:41:30 +0530

    added payment feature

--- src/Routes/payment.js
+++ src/Routes/payment.js
@@ -68,7 +68,8 @@ paymentRouter.post("/payment/webhook", async (req, res) => {
 });
 
 paymentRouter.get("/premium/verify", userAuth, async (req, res) => {
-  const user = res.user;
+  const user = req.user;
+  console.log(req.user);
   if (user.isPremium) {
     return res.send({ isPremium: true });
   }
